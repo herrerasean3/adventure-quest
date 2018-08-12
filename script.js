2e52f16350437592c3364fc99c2bfc0ea53419c2
Sean Herrera <herrerasean3@gmail.com>
2018-08-12 11:23:44 -0400

    Added base elements to HTML and defined current necessary global variables in JS.

---
+++ script.js
@@ -0,0 +1,6 @@
+let storybox = document.querySelector('#storybox');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btn = document.querySelectorAll('.btn');
