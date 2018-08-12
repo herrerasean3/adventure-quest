9a3cbf4670fcf9ced563294a465dd7a783823798
Sean Herrera <herrerasean3@gmail.com>
2018-08-12 11:54:00 -0400

    Implemented eleWipe and elePrint functions.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let btn2 = document.querySelector('#btn2');
 let btn3 = document.querySelector('#btn3');
 let btn4 = document.querySelector('#btn4');
 let btn = document.querySelectorAll('.btn');
+
+//Clears any text or elements inside the target, allowing us to maintain a clean worspace
+function eleWipe(target){
+  target.innerHTML="";
+}
+
+//Sets the contents of the target element to the string provided in content.
+function elePrint(target,content){
+  target.textContent=content;
+}
